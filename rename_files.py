73c98004e2fba03ec7688a678a48c8fbc527b9c0
Casterial <Logan_Shelton@hotmail.com>
2024-01-14 00:12:54 -0800

    Adding a new build
    Supporting excluding "other" "trailer" "movie" or "special" from renaming rules as these folders have a UNIQUE name.

--- rename_files.py
+++ rename_files.py
@@ -1,5 +1,7 @@
 import os
 import time
+
+
 def get_valid_dir():
     directory_path = input("Show Directory: ")
     if os.path.isdir(directory_path):
@@ -12,39 +14,40 @@ def rename_files(dir_path, log_file):
     
     for root, dirs, files in os.walk(dir_path):
         current_folder_name = os.path.basename(root)
+        # Make sure we're only renaming the "season" folders, Plex can have "Other or Trailer"
+        if "season" in current_folder_name.lower():
+            # Get the parent folder of current_folder_name
+            parent_folder_path = os.path.dirname(root)
+            name = os.path.basename(parent_folder_path)
+            # Reset counter for each subdirectory
+            counter = 0  
+            rel_path = os.path.relpath(root, initial_dir)
+            # Fail Safe
+            if rel_path != ".":
+                print(f"\n Renaming {rel_path}")
+                log_file.write(f"********Renaming {rel_path}********\n")
+                
+            for file in files:
+                counter += 1
+                old_file = os.path.join(root, file)
+                file_ext = os.path.splitext(old_file)[1]                
+                if counter < 10:
+                    new_name = f"{name}_{current_folder_name}_0{counter}{file_ext}"
+                else:
+                    new_name = f"{name}_{current_folder_name}_{counter}{file_ext}"
 
-        # Get the parent folder of current_folder_name
-        parent_folder_path = os.path.dirname(root)
-        name = os.path.basename(parent_folder_path)
-        # Reset counter for each subdirectory
-        counter = 0  
-        rel_path = os.path.relpath(root, initial_dir)
-        # Fail Safe
-        if rel_path != ".":
-            print(f"\n Renaming {rel_path}")
-            log_file.write(f"********Renaming {rel_path}********\n")
-            
-        for file in files:
-            counter += 1
-            old_file = os.path.join(root, file)
-            file_ext = os.path.splitext(old_file)[1]                
-            if counter < 10:
-                new_name = f"{name}_{current_folder_name}_0{counter}{file_ext}"
-            else:
-                new_name = f"{name}_{current_folder_name}_{counter}{file_ext}"
+                new_file = os.path.join(root, new_name)
 
-            new_file = os.path.join(root, new_name)
-
-            # Fail Safe: making sure we don't make a file out of the log file we created
-            if "rename_log.txt" not in old_file:
-                try:
-                    os.rename(old_file, new_file)
-                    log_file.write(f"* Renamed: {file} to {new_name}\n")
-                except FileExistsError as e:
-                    print(f"Skipping file {old_file}, it already exists.")
-                except Exception as e:
-                    print(f"{e}")
-                    raise
+                # Fail Safe: making sure we don't make a file out of the log file we created
+                if "rename_log.txt" not in old_file:
+                    try:
+                        os.rename(old_file, new_file)
+                        log_file.write(f"* Renamed: {file} to {new_name}\n")
+                    except FileExistsError as e:
+                        print(f"Skipping file {old_file}, it already exists.")
+                    except Exception as e:
+                        print(f"{e}")
+                        raise
 
 
 def main():
